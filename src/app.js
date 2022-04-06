3dc77fde1011da3323d1b06479281694e9a67246
KarrieD <karriedbrooks@gmail.com>
2022-04-06 14:28:04 -0500

    Showing current weather

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "210412f30f491d1c32937a20641fbe44";
