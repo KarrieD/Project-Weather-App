a6d5df92ad0116b07654d40e988129b1e677767d
KarrieD <karriedbrooks@gmail.com>
2022-04-06 17:15:52 -0500

    Added search engine and default city

--- src/app.js
+++ src/app.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   iconElemnt.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "210412f30f491d1c32937a20641fbe44";
-let city = "Lubbock";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+  let apiKey = "210412f30f491d1c32937a20641fbe44";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Chicago");
