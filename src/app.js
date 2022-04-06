148065014ee1723705acb8154cd52657676b573a
KarrieD <karriedbrooks@gmail.com>
2022-04-06 13:41:50 -0500

    Added current temp

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-let apiKey = "210412f30f491d1c32937a20641fbe44";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}`;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
 
-console.log(apiUrl);
+let apiKey = "210412f30f491d1c32937a20641fbe44";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
